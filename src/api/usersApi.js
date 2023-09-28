33862cecbd35d7882eb9929e656e3ff2247a188b
alsu <9.alsulu@gmail.com>
2023-09-28 16:40:57 +0300

    added cart-button component

--- src/api/usersApi.js
+++
@@ -1,9 +0,0 @@
-import baseApi from "./baseApi";
-
-export const getUsersSearch = (search) => {
-    return baseApi.get(`/users?q=${search}&limit=10&_expand=user`)
-}
-
-export default {
-    getUsersSearch
-}
